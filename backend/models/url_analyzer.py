0003ecb482bbbabaa30867eb4f80ca00e0c7465d
ashendilantha <ashendilantha99@gmail.com>
2025-06-22 13:29:02 +0530

    update file

--- backend/models/url_analyzer.py
+++ backend/models/url_analyzer.py
@@ -66,8 +66,108 @@ class URLAnalyzer:
             return features
         
         def has_ip_address(self, netloc):
+            #Check if url use ip add instead of domain
             ip_pattern = r'\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b'
             return bool(re.search(ip_pattern, netloc))
         
         def count_suspicious_keywords(self, url):
-            return sum
+            #Count suspicious keywords in url
+            return sum(1 for keyword in self.suspicious_keywords if keyword in url)
+        
+        def is_url_shortener(self, domain):
+            shorteners = ['bit.ly', 'tinyurl.com', 't.co', 'goo.gl', 'ow.ly']
+            return domain in shorteners
+        
+        def get_domain_age(self, domain):
+            try:
+                domain_info = whois.whois(domain)
+                if domain_info.creation_date:
+                    creation_date = domain_info.creation_date
+                    if isinstance(creation_date, list):
+                        creation_date = creation_date[0]
+                    age = (datetime.now() - creation_date).days
+                    return age
+            except:
+                 pass
+            return -1
+        
+        def calculate_risk_attack(self, domain):
+            suspicious_chars = ['a', 'e', 'o', 'p', 'c', 'x', 'y']
+            return any(char in domain for char in suspicious_chars)
+        
+        def calculate_risk_score(self, features):
+            #calculate risk base on feature
+            score = 0
+
+            #URL length
+            if features['length'] > 100:
+                score += 20
+            elif features['length'] > 50:
+                score += 10
+
+            #IP usages
+            if features['has_ip']:
+                score += 30
+            
+            #Suspicious keywords
+            score += features['suspicious_keywords'] * 15
+
+            #Subdomain count
+            if features['subdomain_count'] > 2:
+                score += 25
+            
+            #Suspicious TLD
+            if features['has_suspicious_tld']:
+                score += 20
+
+            #url shortener 
+            if features['has_url_shortener']:
+                score += 20
+            
+            #No https
+            if not features['has_https']:
+                score += 10
+
+            #new domain
+            if features['domain_age'] < 30:
+                score += 25
+            
+            #special characters
+            if features['special_chars'] > 10:
+                score += 15
+            
+            #homograph attack
+            if features['has_homograph']:
+                score += 35
+            
+            return min(score, 100)
+        
+        def get_risk_level(self, score):
+            if score >= 70:
+                return "HIGH"
+            elif score >= 40:
+                return "MEDIUM"
+            elif score >= 20:
+                return "LOW"
+            else:
+                return "SAFE"
+            
+        def generate_warnings(self, features):
+            warnings = []
+
+            if features['has_ip']:
+                warnings.append("URL uses IP address instead of domain name")
+            if features['suspicious_keywords'] > 2:
+                warnings.append("Multiple suspicious keywords detected")
+            if features['has_suspicious_tld']:
+                warnings.append("Suspicious Top level domain")
+            if features['has_url_shorteners']:
+                warnings.append("URL shortener detected. destination unknown")
+            if not features['has_https']:
+                warnings.append("No secure HTTPS connection")
+            if 0 <= features['domain_age'] < 30:
+                warnings.append("New domain detected (less than 30 days old)")
+            if features['has_homograph']:
+                warnings.append('Potential homograph attack detected')
+
+            return warnings
